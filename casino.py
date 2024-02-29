f0f264984ebecab18e005dd6a590f726ac494cf6
zhuko <zhukov_ai@outlook.com>
2024-02-29 17:38:35 +0300

    это невозможно

--- casino.py
+++ casino.py
@@ -1,8 +1,17 @@
 import time,os,random
 print("Здравствуйте! У вас 10 монет.")
 time.sleep(1)
-vibor = input("Выберите два числа от 1 до 6 (включительно)")
-stavka = input("Сколько вы поставите монет на эти числа?")
+while True:
+    vibor = input("Выберите одно числа от 1 до 6 (включительно)")
+    vibor_proverka = vibor.isnumeric()
+    if vibor_proverka == True:
+        vibor_int = int(vibor)
+    if vibor_proverka == False or vibor_int > 6 or vibor_int < 1:
+        input("Вы сделали не правильную ставку. Ещё раз выберите одно число от 1 до 6 (включительно)")
+    if vibor_int < 6 and vibor_int > 1:
+        break
+    vibor_str = str(vibor)
+    vibor_str = vibor
 kubik = random.randint(1,6)
 kubiks = str(kubik)
 print("Выпало " + kubiks)
